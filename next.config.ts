7de494ef01d010012229aa9adc5eba90eeec7949
himagone <sunfgirl1975@icloud.com>
2025-04-07 10:43:56 +0900

    update: nextconfig path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: process.env.IS_PUBLIC_REPO ? "" : process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}` : "",
-    assetPrefix: process.env.IS_PUBLIC_REPO ? "" : process.env.REPOSITORY_NAME ? `/${process.env.REPOSITORY_NAME}/` : "",
+    basePath: process.env.IS_PUBLIC_REPO ? `/${process.env.REPOSITORY_NAME}` : "",
+    assetPrefix: process.env.IS_PUBLIC_REPO ? `/${process.env.REPOSITORY_NAME}/` : "",
     base: './',
+    experimental: {
+        optimizePackageImports: ["@chakra-ui/react"],
+    },
 }
 
 module.exports = nextConfig
