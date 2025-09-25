f7247d02929622ae6cb2ff66830b639f48546441
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2025-09-25 19:02:17 +0900

    BASE_PATHが読み込まれない問題に対応

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
+process.env.NEXT_PUBLIC_BASE_PATH = process.env.BASE_PATH || "";
+
 const nextConfig: NextConfig = {
     output: "export",
     basePath: process.env.BASE_PATH,
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
+    },
+    env: {
+        NEXT_PUBLIC_BASE_PATH: process.env.BASE_PATH || "",
     },
 };
 
